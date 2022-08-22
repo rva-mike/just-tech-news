f046906dd791bfc8e3703cc673a91ef2692a9e3f
Mike L <mike.levy.rva@gmail.com>
2022-08-22 16:32:36 -0400

    homepage created

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+
 
 
 // This is so if we make a request to any endpoint that doesn't exist, we'll receive a 404 error indicating we have requested an incorrect resource, another RESTful API practice.
--- server.js
+++ server.js
@@ -1,15 +1,31 @@
+//stylesheet (public folder)
+const path = require('path');
+
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
+
+//handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+// stylesheet
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 // turn on routes
 app.use(routes);
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
