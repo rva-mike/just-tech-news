cbb38c509023c66640ba292f89be0250001be80d
Mike L <mike.levy.rva@gmail.com>
2022-08-29 16:33:51 -0400

    created Dashboard view

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const { User, Post, Vote, Comment } = require('../../models');
+const withAuth = require('../utils/auth');
+
 
 // get all users
 router.get('/', (req, res) => {
@@ -58,7 +60,7 @@ router.get('/:id', (req, res) => {
 
 
 
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
@@ -78,7 +80,7 @@ router.post('/', (req, res) => {
 
 
 
-router.post('/login', (req, res) => {
+router.post('/login', withAuth, (req, res) => {
   User.findOne({
     where: {
       email: req.body.email
@@ -109,7 +111,7 @@ router.post('/login', (req, res) => {
 
 
 
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
   // pass in req.body instead to only update what's passed through
@@ -134,7 +136,7 @@ router.put('/:id', (req, res) => {
 
 
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
   User.destroy({
     where: {
       id: req.params.id
@@ -155,7 +157,7 @@ router.delete('/:id', (req, res) => {
 
 
 
-router.post('/logout', (req, res) => {
+router.post('/logout', withAuth, (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
       res.status(204).end();
